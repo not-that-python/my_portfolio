baea0aa8b3a9f853c4b18fa34a01a1e6ce33f474
not-that-python <avni.k.balan@gmail.com>
2025-07-27 18:59:13 +0100

    added javascript (forgot to add files)

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+// my-portfolio/vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // This tells Vite where your root HTML and static assets are located for development.
+  root: './web',
+
+  // build: {
+  //   // This defines where Vite will output your production build.
+  //   // By default it's 'dist', but here we are saying it will be in 'my-portfolio/dist'
+  //   // relative to the project root, regardless of the 'root' setting.
+  //   outDir: 'dist'
+  // }
+});
